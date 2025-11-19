098cab8ab62cfccfac74203770ec41497aa3ae3c
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2025-11-19 20:53:57 +1000

    Create DucksAndDragonsDay12-1.py

---
+++ 2025/DucksAndDragonsDay12-1.py
@@ -0,0 +1,20 @@
+from collections import deque
+ 
+f = [[int(i) for i in y] for y in open('day12-1in.txt').read().split('\n')]
+
+def compute_reachable(grid, start):
+    rows, cols = len(grid), len(grid[0])
+    r, c = start
+    q = deque([(r, c)])
+    visited = set([(r, c)])    
+    while q:
+        x, y = q.popleft()        
+        for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < rows and 0 <= ny < cols:
+                if (nx, ny) not in visited and grid[nx][ny] <= grid[x][y]:
+                    visited.add((nx, ny))
+                    q.append((nx, ny))
+    return visited
+ 
+print(len(compute_reachable(f,(0,0))))
